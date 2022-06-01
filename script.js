e3307fc102115727b88252195802eec708efd7bc
aaronlrv <aaronvitharanage@gmail.com>
2022-06-01 20:07:04 +0400

    Add rough outline of button functionality

--- script.js
+++ script.js
@@ -30,7 +30,66 @@ console.log (playerSelection)
         }
     }
     
-    function rounds (){
+
+    function rounds () {
+            let computerSelection = computerPlay()
+            let playerSelection =  playerInput.toLowerCase()
+            
+            rps (playerSelection, computerSelection)
+
+
+        }
+
+let rock = document.getElementsByClassName("rock")
+let paper = document.getElementsByClassName("paper")
+let scissors = document.getElementsByClassName("scissors")
+
+let buttons = document.querySelectorAll("button")
+buttons.forEach(button => {
+    button.addEventListener("click", classification(button))
+    })
+
+
+function classification (button) {
+        if(buttons.className === "rock"){
+                playerSelection === "rock"
+                    console.log(playerSelection)
+        } else if (buttons.className === "paper"){
+                playerSelection === "paper"
+                console.log(playerSelection)
+        } else {
+                playerSelection === "scissors"
+                console.log(playerSelection)
+                }
+            }
+
+
+
+
+/*
+buttons.addEventListener('click', classification)
+
+
+function classification (rps) {
+    if(button.className === "rock"){
+        playerSelection === "rock"
+        console.log(playerSelection)
+} else if (button.className === "paper"){
+        playerSelection === "paper"
+        console.log(playerSelection)
+} else {
+        playerSelection === "scissors"
+        console.log(playerSelection)
+        }
+    }    
+
+rounds()
+
+*/
+
+
+
+    /*function rounds (){
         for (let i = 0; i < 5; i++) {
             let computerSelection = computerPlay()
             let playerInput =  window.prompt("Rock, Paper, Or Scissors?")
@@ -43,5 +102,39 @@ console.log (playerSelection)
 
 
     }
+    
+rounds()*/
 
-rounds()
+/*
+buttons.forEach(classification)
+
+function classification (callback) {
+    buttons.onclick = function (){
+        if(buttons.className === "rock"){
+                playerSelection === "rock"
+                    console.log(playerSelection)
+        } else if (buttons.className === "paper"){
+                playerSelection === "paper"
+                console.log(playerSelection)
+        } else {
+                playerSelection === "scissors"
+                console.log(playerSelection)
+                }
+            }
+}*/
+
+/*
+buttons.forEach(buttons.onclick = () => {
+    if(buttons.className === "rock"){
+            playerSelection === "rock"
+                console.log(playerSelection)
+    } else if (buttons.className === "paper"){
+            playerSelection === "paper"
+            console.log(playerSelection)
+    } else {
+            playerSelection === "scissors"
+            console.log(playerSelection)
+            }
+        })
+
+*/
