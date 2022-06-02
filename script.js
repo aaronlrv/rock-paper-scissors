6dc34be974531aa5e977d03dfb80b167ff95283f
aaronlrv <aaronvitharanage@gmail.com>
2022-06-02 12:45:40 +0400

    Comments for readability along with play rounds function to keep track of the number of rounds

--- script.js
+++ script.js
@@ -6,57 +6,77 @@ let scissors = document.getElementsByClassName("scissors")
 
 let buttons = document.querySelectorAll("button")
 
-let playerSelection = buttons.forEach(button => {
-    button.addEventListener("click", classification)
+let playedRounds = 0 // the amount of rounds already played
+buttons.forEach((button) => {
+    button.addEventListener("click", (evt) => {
+        console.log(evt)
+        // first, check if we've already played 5 rounds
+        // if so, `return` will prevent this function from continuing
+        // this means that playedRounds won't increment either, although that won't matter
+        // so, this basically means the button won't do anything after 5 games
+        if (playedRounds >= 5) {
+            document.querySelector("#results").innerHTML = "You already played 5 games, no more for you!"
+            return
+        }
+
+        let playerSelection = evt.target.className
+
+        rps(playerSelection, computerSelection());
+        // ^ another way to do this would be:
+        //     let computerSelection = computerPlay()
+        //     rps(playerSelection, computerSelection)
+        // but there's no reason to make yet another variablecalled computerSelection when we can just put it there directly
+
+        playedRounds++ // increment playedRounds since we just played a round
+        score()
     })
+})
 
-function classification (evt) {
-    let playerChoice = evt.target.className;
-    rps(playerChoice, computerPlay())
-      }
-
-console.log(playerSelection)
 /* Randomizes computer inputs */
-function computerPlay () {
+function computerSelection() {
     return game [Math.floor(Math.random () * game.length)]
 }
-let computerSelection = computerPlay()
+
+let playerScore = 0
+let computerScore = 0
+function score() {
+    if (playerSelection === computerSelection){
+        computerScore = computerScore + 0
+        playerScore = playerScore + 0
+    }
+    else if (resultTitle = "You win!"){
+        playerScore = playerScore + 1
+    } else {
+        computerScore = computerScore + 1
+    }
+ console.log (computerScore)
+ console.log (playerScore)   
+}
 
 /* Win & Lose conditions of RPS as well as prints results */
 function rps(playerSelection, computerSelection) {
-    let results = document.getElementsByClassName("results")
-    results.innerHTML = `player: ${playerSelection}`
-    results.innerHTML = `computer: ${computerPlay}`
-
-
-    if (playerSelection === computerSelection){
-            results.innerHTML = "It's a tie!"
-        }
-
-    else if ((playerSelection === "rock" && computerSelection === "scissors") ||
-            (playerSelection === "paper" && computerSelection === "rock") ||
-            (playerSelection === "scissors" && computerSelection === "paper")) {
-            results.innerHTML = "You win!"
-        } else {
-            results.innerHTML = "You Lose!"
-        }
+    let resultTitle;
+    if (playerSelection === computerSelection) {
+        resultTitle = "It's a tie!"
+    } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")) {
+        resultTitle = "You win!"
+    } else {
+        resultTitle = "You lose!"
     }
-    
 
-/* Determines the amount of rounds played */            
-function rounds (){
-    for (let i = 0; i < 5; i++) {
-        let computerSelection = computerPlay()
-        let playerInput =  playerSelection
-            
-        rps (playerSelection, computerSelection)
-
-
-        }
-
-
-    }
-rounds()
+    let results = document.querySelector("#results")
+    results.innerHTML = `
+        <p>${resultTitle}</p>
+        <hr>
+        <p>
+            player: ${playerSelection}
+            <br>
+            computer: ${computerSelection}
+        </p>
+    ` // this is a closing quote, not a typo
+}
 
 
 /*
